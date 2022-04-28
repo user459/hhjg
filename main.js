697d5ee515b4616745feb24d9ede84a518e94833
user459 <86778583+user459@users.noreply.github.com>
2022-04-28 19:02:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var speechrecognition=window.webkitSpeechRecognition;
+var recognition=new speechrecognition();
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML=content;
+}
